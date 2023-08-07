7b1c34aa2b7b117a10f843dd64375018d23212b2
Anastasiia-Bondarchuk <125996065+AnastasiaBndr@users.noreply.github.com>
2023-08-07 14:44:16 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -11,6 +11,6 @@ export function fetchBreed() {
       return axios.get(URL+API_KEY)
       .then(resp=>{return resp.data})
       .then(cats=>{return cats.filter(cat => cat.image?.url != null || cat.id === 'mala')})
-      .catch(Notiflix.Report.info("Error!", "Cats ran away somewhere..", "Ok"))
+      .catch(err => Notiflix.Report.info("Error!", "Cats ran away somewhere..", "Ok"))
     
   }
